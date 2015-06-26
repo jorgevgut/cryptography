15682a4e202be91762ddf4b6b42d04a2aac5bbd6
xymind <jogeavi914@gmail.com>
2015-06-26 18:50:28 -0500

    cipher vigenere in python

---
+++ caesar/encrypt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def main():
+    ptext = input("please enter plain-text\n")
+    key = input("now enter your key\n")
+    ctext = get_cipher(key,ptext)
+    print(ctext)
+
+def get_cipher(key,ptext):
+    ctext = ""
+    ksize = len(key)
+    for index,val in enumerate(ptext):
+        c = ord(val) ^ ord(key[index % ksize])
+        ctext += str(chr(c))
+
+    return ctext
+
+
+main()
